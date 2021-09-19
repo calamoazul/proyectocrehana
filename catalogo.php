862601267d7d1a87fff987ae4052c1bb6eb717fb
CalamoAzul <info@calamoazul.com>
2021-09-19 03:03:03 +0200

    Diseño catalogo finalizado

--- catalogo.php
+++ catalogo.php
@@ -3,6 +3,7 @@ include ("header.php");
 ?>
    <div class="catalogo"></div>
    <h1>Catálogo de Ediciones Hati</h1>
+   <section class="muestra">
    <img class="libro" src="images/hijosdeldestino.jpg">
    <form method=post action="hijos-del-destino.php">
      <input class="botoncatalogo" type="submit" value="Información">
@@ -15,4 +16,5 @@ include ("header.php");
    <form method=post action="ishi-va-a-la-escuela.php">
      <input class="botoncatalogo" type="submit" value="Información">
    </form>
+   <section class="muestra">
 <?php include ("footer.php");?>
