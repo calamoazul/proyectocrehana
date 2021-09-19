bd6969015d79188b233f999924391959714fcb04
CalamoAzul <info@calamoazul.com>
2021-09-19 02:30:14 +0200

    Creando encabezados

--- catalogo.php
+++ catalogo.php
@@ -1,9 +1,9 @@
 <?php
-require 'functions.php';
-get_header() ?>
+include ("header.php");
+?>
    <div class="catalogo"></div>
    <h1>Catálogo de Ediciones Hati</h1>
-   <img class="libro" src="images/hijosdeldestino.png">
+   <img class="libro" src="images/hijosdeldestino.jpg">
    <form method=post action="hijos-del-destino.php">
      <input class="botoncatalogo" type="submit" value="Información">
    </form>
@@ -11,7 +11,8 @@ get_header() ?>
    <form method=post action="prime-time.php">
      <input class="botoncatalogo" type="submit" value="Información">
    </form>
-   <img class="libro" src="images/ishi.png">
+   <img class="libro" src="images/ishi.jpg">
    <form method=post action="ishi-va-a-la-escuela.php">
      <input class="botoncatalogo" type="submit" value="Información">
    </form>
+<?php include ("footer.php");?>
---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+<p class="pfooter">Realizado por Cálamo Azul</p>
+<p class="pfooter">©2021 Ediciones Hati</p>
+</footer>
+</body>
