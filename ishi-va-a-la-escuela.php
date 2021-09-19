7ee400019dfc6e1d6d561080ebd707004631f54c
CalamoAzul <info@calamoazul.com>
2021-09-19 03:39:40 +0200

    Se ha terminado de añadir todo el catálogo

---
+++ ishi-va-a-la-escuela.php
@@ -0,0 +1,16 @@
+<?php
+include ("header.php"); ?>
+<div class="catalogo">
+<h1>Ishi va a la escuela</h1>
+  <section class="muestra">
+    <img class="libro" src="images/ishi.jpg">
+    <ul>
+      <li><strong>Autoras:</strong> Ana Pérez de Cámara e Isabel de las Heras Vidal</li>
+      <li><strong>Sinopsis:</strong> Ishi es una gárgola especial que comienza su primer día de colegio. Está muy emocionada por conocer a su maestra y a sus nuevos compañeros. Sin embargo tendrá que afrontar un nuevo reto: enseñar a su clase a hablar como ella.</li>
+    </ul>
+    <form method="post" action="https://www.edicioneshati.com/product/ishi-va-a-la-escuela-literatura-infantil/">
+      <input type="submit" class="botonficha" value="Ir a la editorial">
+</section>
+</div>
+<?php
+include ("footer.php");?>
---
+++ prime-time.php
@@ -0,0 +1,22 @@
+<?php
+include ("header.php"); ?>
+<div class="catalogo">
+<h1>Prime Time</h1>
+  <section class="muestra">
+    <img class="libro" src="images/primetime.png">
+    <ul>
+      <li><strong>Autor:</strong> Raúl Caronte</li>
+      <li><strong>Sinopsis:</strong>Hace quince años, un psicópata irrumpió en una casa de Liverpool e hizo elegir a Desmond Hayden a cuál de sus dos hijos, Eileen o Jackson, debía asesinar. Él sacrificó a su hija, lo que supuso un lastre que la familia jamás fue capaz de superar.
+
+En el Más Allá, donde se avivan los sentimientos más perversos de los muertos, Hache tiene una idea para lanzar un programa de televisión que batirá todos los récords de audiencia.
+
+Eileen Hayden será la protagonista de un nuevo reality show que la devolverá al mundo de los vivos con el objetivo de consumar su venganza. Deberá matar al hombre que la asesinó, al padre que la sentenció y al hermano que fue salvado en su lugar. A cambio, podrá vivir el resto de sus días reencarnada en su nuevo cuerpo.
+
+Jackson Hayden, quien ha pasado los últimos quince años intentando llegar hasta el asesino, ignora que es la futura víctima de su hermana. Aunque les una la sangre, Eileen tiene un contrato que cumplir porque, si rehúsa, volverá a abandonar el mundo de los vivos.</li>
+    </ul>
+    <form method="post" action="https://www.edicioneshati.com/product/prime-time-raul-caronte/">
+      <input type="submit" class="botonficha" value="Ir a la editorial">
+</section>
+</div>
+<?php
+include ("footer.php");?>
